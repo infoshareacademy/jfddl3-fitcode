aee89322862b3e6bc687d4308792a34222665598
Wojtek Pyda <w.pyda@poczta.onet.pl>
2017-12-05 16:49:51 +0100

    footer colors added

--- nav.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function menu(){
-    $('.nav-hamburger').click(function() {
-        $('.navigation').toggleClass('appears').slideToggle(300);
-    });
-
-    $('.nav-hamburger').click(function() {
-        $(this).toggleClass('transform');
-    });
-});
