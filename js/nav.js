0ea2f8cd2704be73a680a278da3b75f10caee642
Wojtek Pyda <w.pyda@poczta.onet.pl>
2017-12-14 17:08:28 +0100

    nav marks added

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
 
-    $('.nav-hamburger').click(function() {
+    $('.nav-hamburger').on('click', function() {
+
         $('.navigation').slideToggle(300);
         $(this).toggleClass('transform');
+
     });
+
+
+    var navEl = $('.navigation ul li a');
+
+    navEl.on('click', function colors(e) {
+
+        navEl.removeClass('active');
+        $(this).toggleClass('active');
+
+        navEl.addClass('nav-nav-color');
+        $(this).toggleClass('nav-nav-color');
+    });
+
+
+
 });
+
