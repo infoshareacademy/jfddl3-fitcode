3d73f52ec14126368f1a5399c19584d526fd7c0c
Wojtek Pyda <w.pyda@poczta.onet.pl>
2017-12-18 17:00:09 +0100

    inview added

--- js/nav.js
+++ js/nav.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 
     });
 
+/*
 
     var navEl = $('.navigation ul li a');
 
@@ -19,10 +20,72 @@ $(document).ready(function() {
         $(this).toggleClass('nav-nav-color');
     });
 
+*/
+
+// remove class from others except active one
+
+    inView('#start')
+        .on('enter', function() {
+            $('.nav-start').addClass('active')
+            $('.nav-start').removeClass('nav-nav-color')
+        })
+
+        .on('exit', function() {
+            $('.nav-start').removeClass('active')
+            $('.nav-start').addClass('nav-nav-color')
+        })
+
+    inView('#what')
+        .on('enter', function() {
+            $('.nav-fit').addClass('active')
+            $('.nav-fit').removeClass('nav-nav-color')
+        })
+
+        .on('exit', function() {
+            $('.nav-fit').removeClass('active')
+            $('.nav-fit').addClass('nav-nav-color')
+        })
+
+    inView('#newsletter')
+        .on('enter', function() {
+            $('.nav-prem').addClass('active')
+            $('.nav-prem').removeClass('nav-nav-color')
+        })
+
+        .on('exit', function() {
+            $('.nav-prem').removeClass('active')
+            $('.nav-prem').addClass('nav-nav-color')
+        })
+
+    inView('#details')
+        .on('enter', function() {
+            $('.nav-det').addClass('active')
+            $('.nav-det').removeClass('nav-nav-color')
+        })
+
+        .on('exit', function() {
+            $('.nav-det').removeClass('active')
+            $('.nav-det').addClass('nav-nav-color')
+        })
+
+    inView('#about')
+        .on('enter', function() {
+            $('.nav-us').addClass('active')
+            $('.nav-us').removeClass('nav-nav-color')
+        })
+
+        .on('exit', function() {
+            $('.nav-us').removeClass('active')
+            $('.nav-us').addClass('nav-nav-color')
+        })
+});
+
+
+    /*
     var os = $('.logo a img').offset();
     console.log(os);
 
-    /*Offset TOP
+    Offset TOP
 
     START:        0
 
@@ -33,7 +96,7 @@ $(document).ready(function() {
     SZCZEGÓŁY:    2036
 
     EKIPA:        2947
-     */
+
     var a = $('.navigation ul li nth-child(2) a');
     if (os.offsetTop > 726 && os.offsetTop < 1394) {
 
@@ -42,6 +105,5 @@ $(document).ready(function() {
             a.removeClass('active')
         }
     }
-
-});
+*/
 
